88ad4cbfc82a4613f547d264b4e672d250e63fe2
AnaGuiraldelo32 <106742141+AnaGuiraldelo32@users.noreply.github.com>
2024-09-05 17:39:44 -0300

    perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,115 @@
+ export const perguntas = [
+    {
+        enunciado: "1.Qual tipo de protagonista você mais gosta?",
+        alternativas: [
+            {
+                texto: "A) Um detetive astuto ou um investigador que resolve crimes complexos",
+                afirmacao: [
+                    " Mistério/Thriller – Você gosta de histórias cheias de suspense, enigmas e reviravoltas."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "B) Um romântico apaixonado ou um herói que busca um propósito",
+                afirmacao: [
+                    "Romance/Drama – Você se encanta com histórias emocionantes e complexas sobre amor e relacionamentos."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "2. Qual é o tema que mais te interessa??",
+        alternativas: [
+            {
+                texto: "Fantasia e aventura épica",
+                afirmacao: [
+                    " Mistério/Thriller – Você gosta de histórias cheias de suspense, enigmas e reviravoltas.",
+    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Romance e drama pessoal",
+                afirmacao: [
+                    "Romance/Drama – Você se encanta com histórias emocionantes e complexas sobre amor e relacionamentos."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: " Como você prefere a narrativa de um livro?",
+        alternativas: [
+            {
+                texto: "Detalhada e informativa, com uma análise profunda de eventos e contextos",
+                afirmacao: [
+                    " Mistério/Thriller – Você gosta de histórias cheias de suspense, enigmas e reviravoltas."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Descritiva e emocional, focando em relacionamentos e crescimento pessoal",
+                afirmacao: [
+                    "Romance/Drama – Você se encanta com histórias emocionantes e complexas sobre amor e relacionamentos."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: " Qual desses passatempos você mais gosta?",
+        alternativas: [
+            {
+                texto: " Resolver quebra-cabeças e enigmas",
+                afirmacao: [
+                    " Mistério/Thriller – Você gosta de histórias cheias de suspense, enigmas e reviravoltas."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Assistir a romances e dramas em filmes ou séries",
+                afirmacao: [
+                    "Romance/Drama – Você se encanta com histórias emocionantes e complexas sobre amor e relacionamentos."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual desses elementos você prefere em uma história?",
+        alternativas: [
+            {
+                texto: "Mistérios a serem desvendados e crimes a serem solucionados",
+                afirmacao: [
+                    " Mistério/Thriller – Você gosta de histórias cheias de suspense, enigmas e reviravoltas."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Relações complexas e histórias de amor intensas",
+                afirmacao: [
+                  "Romance/Drama – Você se encanta com histórias emocionantes e complexas sobre amor e relacionamentos"
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você descreveria sua leitura ideal?",
+        alternativas: [
+            {
+                texto: " Um romance envolvente que toca seu coração",
+                afirmacao: [
+                    "Romance/Drama – Você se encanta com histórias emocionantes e complexas sobre amor e relacionamentos."
+                ],
+            },
+            {
+                texto: "Um suspense emocionante que desafia sua mente",
+                afirmacao: [
+                    " Mistério/Thriller – Você gosta de histórias cheias de suspense, enigmas e reviravoltas."
+                ],
+            },
+        ]
+    },
+];
